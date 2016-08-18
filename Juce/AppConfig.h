d78c2d3bf9a723d8bb6772c0431f3989739f06ea
Martin Palko <jeeprubi@hotmail.com>
2016-08-19 00:43:29 +0200

    Initial commit of current dependencies from the main Vanguard repo.

---
+++ Juce/AppConfig.h
@@ -0,0 +1,35 @@
+/*
+    Any commented-out settings will assume their default values.
+
+*/
+
+#ifndef __JUCE_APPCONFIG_H4NHFR__
+#define __JUCE_APPCONFIG_H4NHFR__
+//==============================================================================
+#define JUCE_MODULE_AVAILABLE_juce_core      1
+
+//==============================================================================
+// juce_core flags:
+
+#ifndef    JUCE_FORCE_DEBUG
+ //#define JUCE_FORCE_DEBUG
+#endif
+
+#ifndef    JUCE_LOG_ASSERTIONS
+ //#define JUCE_LOG_ASSERTIONS
+#endif
+
+#ifndef    JUCE_CHECK_MEMORY_LEAKS
+ //#define JUCE_CHECK_MEMORY_LEAKS
+#endif
+
+#ifndef    JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
+ //#define JUCE_DONT_AUTOLINK_TO_WIN32_LIBRARIES
+#endif
+
+#ifndef    JUCE_INCLUDE_ZLIB_CODE
+ //#define JUCE_INCLUDE_ZLIB_CODE
+#endif
+
+
+#endif  // __JUCE_APPCONFIG_H4NHFR__
